bd7dfa7baeaa4c0ae13f495102dcc0e23bb364a0
m010 <m010@benner.com.br>
2018-08-16 11:54:30 -0300

    Add DAL

---
+++ WebAppLayer/DAL/WildSaverContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAppLayer.Models;
+
+namespace WebAppLayer.DAL
+{
+    public class WildSaverContext : DbContext
+    {
+        internal DbSet<Denuncia> Denuncias { get; set; }
+        internal DbSet<Usuario> Usuarios { get; set; }
+}
+}
