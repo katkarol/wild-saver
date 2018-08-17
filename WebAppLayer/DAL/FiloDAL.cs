76bfa48b6de827f92f2ec7398c1f612fee5cbb82
m010 <m010@benner.com.br>
2018-08-17 11:51:57 -0300

    Iniciando com CRUD

---
+++ WebAppLayer/DAL/FiloDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.DAL
+{
+    public class FiloDAL : IDisposable
+    {
+        public FiloDAL()
+        {
+            contexto = new WildSaverContext();
+        }
+        private WildSaverContext contexto;
+
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+    }
+}
