4f2a6a0ffeede3dd31f392dc95adc2419cb4cefa
m010 <m010@benner.com.br>
2018-08-20 11:51:28 -0300

    Adicionando migration de genero

--- WebAppLayer/Migrations/20180820144723_Genero.Designer.cs
+++ WebAppLayer/Migrations/20180820144723_Genero.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppLayer.DAL;
 
 namespace WebAppLayer.Migrations
 {
     [DbContext(typeof(WildSaverContext))]
-    partial class WildSaverContextModelSnapshot : ModelSnapshot
+    [Migration("20180820144723_Genero")]
+    partial class Genero
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAppLayer/Migrations/20180820144723_Genero.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppLayer.Migrations
+{
+    public partial class Genero : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Generos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    FamiliaID = table.Column<int>(nullable: true),
+                    NomePopular = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Generos", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Generos_Familias_FamiliaID",
+                        column: x => x.FamiliaID,
+                        principalTable: "Familias",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Generos_FamiliaID",
+                table: "Generos",
+                column: "FamiliaID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Generos");
+        }
+    }
+}
