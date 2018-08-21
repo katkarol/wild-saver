6600d365023a465385420d4931233a4b87f405ff
m010 <m010@benner.com.br>
2018-08-21 11:31:20 -0300

    Adicioanando a classe pessoa

---
+++ WebAppLayer/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models
+{
+    public class Pessoa
+    {
+        public string NomeCompleto { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Endereco Endereco { get; set; }
+        public PapelUsuario PapelUsuario { get; set; }
+        public string CPF { get; set; }
+        public Usuario Usuario { get; set; }
+
+
+    }
+}
