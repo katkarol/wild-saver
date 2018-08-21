07ef5d621c256efe908315ace36b0adaf011393a
m010 <m010@benner.com.br>
2018-08-21 11:54:20 -0300

    Adicionando enums em pasta separada

---
+++ WebAppLayer/Models/Enum/PapelUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models.Enum_s
+{
+    public enum PapelUsuario
+    {
+        Admin,
+        Funcionário,
+        Usuário
+    }
+}
