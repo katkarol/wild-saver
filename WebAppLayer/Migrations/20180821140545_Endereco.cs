9a6f0e8d17b950ed5874ed0e3ea520e1a0ec7cb9
m010 <m010@benner.com.br>
2018-08-21 11:30:18 -0300

    Adicionando migration de endereco

---
+++ WebAppLayer/Migrations/20180821140545_Endereco.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppLayer.Migrations
+{
+    public partial class Endereco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enderecos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Rua = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    UF = table.Column<string>(nullable: true),
+                    Pais = table.Column<string>(nullable: true),
+                    CEP = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enderecos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enderecos");
+        }
+    }
+}
