35cd4b453349972bddffd9df3e33e16e1c866b8f
m010 <m010@benner.com.br>
2018-08-22 09:40:54 -0300

    Adicionando a migration de usuario

---
+++ WebAppLayer/Migrations/20180822123428_Usuarios.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppLayer.Migrations
+{
+    public partial class Usuarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Login = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    NomeCompleto = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    EnderecoID = table.Column<int>(nullable: true),
+                    PapelUsuario = table.Column<int>(nullable: false),
+                    CPF = table.Column<string>(nullable: true),
+                    UsuarioID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.UsuarioID);
+                    table.ForeignKey(
+                        name: "FK_Pessoas_Enderecos_EnderecoID",
+                        column: x => x.EnderecoID,
+                        principalTable: "Enderecos",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pessoas_Usuarios_UsuarioID",
+                        column: x => x.UsuarioID,
+                        principalTable: "Usuarios",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoas_EnderecoID",
+                table: "Pessoas",
+                column: "EnderecoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
