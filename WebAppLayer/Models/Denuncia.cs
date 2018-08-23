dd958067bb923fc8289a1a9351c5345b4e0ca468
m010 <m010@benner.com.br>
2018-08-23 11:38:15 -0300

    Adicionando a proppriedade Usuario e mudando o no me da propreidade para data insercao

--- WebAppLayer/Models/Denuncia.cs
+++ WebAppLayer/Models/Denuncia.cs
@@ -8,7 +8,8 @@ namespace WebAppLayer.Models
     public class Denuncia
     {
         public int ID { get; set; }
-        public DateTime DataHoraDenuncia { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime DataInsercao { get; set; }
         public DateTime DataDaDenuncia { get; set; }
         public Endereco Local { get; set; }
         public Animal Animal { get; set; }
