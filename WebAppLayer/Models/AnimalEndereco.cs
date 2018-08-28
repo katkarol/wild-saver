053fbe9bfb7c4952527a5485ad2fbd90d46b351c
m010 <m010@benner.com.br>
2018-08-28 11:57:01 -0300

    adicionando os models intermediarios

---
+++ WebAppLayer/Models/AnimalEndereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models
+{
+    public class AnimalEndereco
+    {
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; }
+        public int EnderecoId { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
---
+++ WebAppLayer/Models/DenunciaAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models
+{
+    public class DenunciaAnimal
+    {
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; }
+        public int DenunciaId { get; set; }
+        public Denuncia Denuncia { get; set; }
+    }
+}
