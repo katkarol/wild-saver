5f51f434339ec535ac64ba65a14709e14cfe7739
m010 <m010@benner.com.br>
2018-09-11 08:29:45 -0300

    Adicionando a classe regiao

---
+++ WebAppLayer/Models/AnimalRegiao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models
+{
+    public class AnimalRegiao
+    {
+        public int AnimalId { get; set; }
+        public Animal Animal { get; set; }
+        public int RegiaoId{ get; set; }
+        public Regiao Regiao { get; set; }
+    }
+}
---
+++ WebAppLayer/Models/Regiao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models
+{
+    public class Regiao
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "A cidade deve ser informada")]
+        public string Cidade { get; set; }
+        [Required(ErrorMessage = "O estado deve ser informado")]
+        public string UF { get; set; }
+        [Required(ErrorMessage = "O país deve ser informado")]
+        public string Pais { get; set; }
+        
+    }
+}
