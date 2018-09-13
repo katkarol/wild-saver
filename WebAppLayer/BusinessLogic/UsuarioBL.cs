06688b791ff1635f63a7833b3cf3a13e68fc4c51
m010 <m010@benner.com.br>
2018-09-13 09:35:01 -0300

    Adicionando a regra de negocio

---
+++ WebAppLayer/BusinessLogic/UsuarioBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppLayer.DAL;
+using WebAppLayer.Models;
+using WebAppLayer.Models.Enums;
+
+namespace WebAppLayer.BusinessLogic
+{
+    public class UsuarioBL
+    {
+        public static void ValidacoesCriarConta(Usuario usuario)
+        {
+            usuario.PessoaUsuaria.CPF.Replace(".", "").Replace("-", "");
+            if (usuario.PessoaUsuaria.CPF.Length != 11)
+            {
+                throw new Exception("CPF Inválido!");
+            }
+            if (usuario.PapelUsuario == PapelUsuario.Usuario && usuario.PessoaUsuaria.DataNascimento > DateTime.Now.AddYears(-14))
+            {
+                throw new Exception("Idade Inválida! Para se cadastrar deve ser maior de 14 anos.");
+            }
+            else if (usuario.PessoaUsuaria.DataNascimento > DateTime.Now.AddYears(-18) && usuario.PapelUsuario == PapelUsuario.Funcionario)
+            {
+                throw new Exception("Idade Inválida! Para cadastrar funcionario deve ser maior de 18 anos.");
+            }
+            UsuarioDAL.Adicionar(usuario);
+
+        }
+        public static void ValidacoesLogin(Usuario usuario)
+        {
+            Usuario u = UsuarioDAL.BuscarUsuarioSenha(usuario);
+            if (u.Login == usuario.Login)
+            {
+                throw new Exception("O usuario e/ou a senha estão incorretos");
+            }
+            if (u.Senha == usuario.Senha)
+            {
+                throw new Exception("O usuario e/ou a senha estão incorretos");
+            }
+        }
+    }
+}
