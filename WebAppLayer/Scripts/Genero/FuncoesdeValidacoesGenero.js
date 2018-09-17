99fc38ba1c2baf1693aa3f3568cf122941951949
m010 <m010@benner.com.br>
2018-09-17 11:21:32 -0300

    Adicionando o js do genero que faltava

---
+++ WebAppLayer/Scripts/Genero/FuncoesdeValidacoesGenero.js
@@ -0,0 +1,13 @@
+﻿function NomeValido(nome) {
+    if (nome.length < 3 || nome > 30) {
+        return false;
+    }
+    return true;
+}
+
+function NomePopularValido(nomePopular) {
+    if (nomePopular < 3 || nomePopular > 40) {
+        return false;
+    }
+    return true;
+}
