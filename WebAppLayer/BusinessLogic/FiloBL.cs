9eefdfd752942dcc6ca4e01d86b72d6897f1f6c0
m010 <m010@benner.com.br>
2018-09-17 10:04:10 -0300

    Validando Filos

---
+++ WebAppLayer/BusinessLogic/FiloBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppLayer.DAL;
+using WebAppLayer.Models;
+
+namespace WebAppLayer.BusinessLogic
+{
+    public class FiloBL
+    {
+        public static void AdicionarFilo(Filo filo)
+        {
+            FiloDAL.Adicionar(filo);
+        }
+    }
+}
---
+++ WebAppLayer/Scripts/Filo/ValidacoesFilo.js
@@ -0,0 +1,33 @@
+﻿function ObtemInformacoesFormulario() {
+    var Filo = {
+        Nome: document.querySelector("#Nome").value,
+        NomePopular: document.querySelector("#NomePopular").value,
+    }
+    return Filo
+
+}
+function ValidacaoCampos(classe) {
+
+    var erros = document.querySelectorAll(".text-danger");
+    for (var i = 0; i < erros.length; i++) {
+        erros[i].innerHTML = "";
+
+        if (!NomeValido(classe.nome)) {
+            erros[0].textContent = "Nome inváido";
+        }
+        if (!NomePopularValido(classe.NomePopular)) {
+            erros[1].textContent = "Nome popular inválido";
+        }
+    }
+
+
+
+    for (var i = 0; i < erros.length; i++) {
+        if (erros[i].textContent != "") {
+            return false;
+        }
+    }
+
+    return true;
+
+}
