e08e4cfb9473695be0d947c4fc4aeee0f560c846
m010 <m010@benner.com.br>
2018-09-20 11:40:15 -0300

    Adicionando estado da  denuncia no db

---
+++ WebAppLayer/Migrations/20180920143353_estado-denuncia.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppLayer.Migrations
+{
+    public partial class estadodenuncia : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnimalID",
+                table: "Regioes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EstadoDenuncia",
+                table: "Denucias",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Regioes_AnimalID",
+                table: "Regioes",
+                column: "AnimalID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Regioes_Animais_AnimalID",
+                table: "Regioes",
+                column: "AnimalID",
+                principalTable: "Animais",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Regioes_Animais_AnimalID",
+                table: "Regioes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Regioes_AnimalID",
+                table: "Regioes");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalID",
+                table: "Regioes");
+
+            migrationBuilder.DropColumn(
+                name: "EstadoDenuncia",
+                table: "Denucias");
+        }
+    }
+}
---
+++ WebAppLayer/Models/Enums/EstadoDenuncia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLayer.Models.Enums
+{
+    public enum EstadoDenuncia
+    {
+        Aberto,
+        EmAndamento,
+        Atendida,
+        Arquivada,
+        Cancelada
+    }
+}
