230f5a9c5e24e6e59de3f738964e44317c1512de
m010 <m010@benner.com.br>
2018-09-21 11:22:06 -0300

    alterando o nome da tabela(escrito errado)

---
+++ WebAppLayer/Migrations/20180921122703_denuncias.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppLayer.Migrations
+{
+    public partial class denuncias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Denucias_Enderecos_LocalID",
+                table: "Denucias");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Denucias_Usuarios_UsuarioID",
+                table: "Denucias");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_DenunciaAnimal_Denucias_DenunciaId",
+                table: "DenunciaAnimal");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Denucias",
+                table: "Denucias");
+
+            migrationBuilder.RenameTable(
+                name: "Denucias",
+                newName: "Denuncias");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Denucias_UsuarioID",
+                table: "Denuncias",
+                newName: "IX_Denuncias_UsuarioID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Denucias_LocalID",
+                table: "Denuncias",
+                newName: "IX_Denuncias_LocalID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Denuncias",
+                table: "Denuncias",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DenunciaAnimal_Denuncias_DenunciaId",
+                table: "DenunciaAnimal",
+                column: "DenunciaId",
+                principalTable: "Denuncias",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Denuncias_Enderecos_LocalID",
+                table: "Denuncias",
+                column: "LocalID",
+                principalTable: "Enderecos",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Denuncias_Usuarios_UsuarioID",
+                table: "Denuncias",
+                column: "UsuarioID",
+                principalTable: "Usuarios",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DenunciaAnimal_Denuncias_DenunciaId",
+                table: "DenunciaAnimal");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Denuncias_Enderecos_LocalID",
+                table: "Denuncias");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Denuncias_Usuarios_UsuarioID",
+                table: "Denuncias");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Denuncias",
+                table: "Denuncias");
+
+            migrationBuilder.RenameTable(
+                name: "Denuncias",
+                newName: "Denucias");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Denuncias_UsuarioID",
+                table: "Denucias",
+                newName: "IX_Denucias_UsuarioID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Denuncias_LocalID",
+                table: "Denucias",
+                newName: "IX_Denucias_LocalID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Denucias",
+                table: "Denucias",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Denucias_Enderecos_LocalID",
+                table: "Denucias",
+                column: "LocalID",
+                principalTable: "Enderecos",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Denucias_Usuarios_UsuarioID",
+                table: "Denucias",
+                column: "UsuarioID",
+                principalTable: "Usuarios",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DenunciaAnimal_Denucias_DenunciaId",
+                table: "DenunciaAnimal",
+                column: "DenunciaId",
+                principalTable: "Denucias",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
