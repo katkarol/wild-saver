2c8d4048c92350f6de55f54e6f1d6306b5d13ecd
m010 <m010@benner.com.br>
2018-09-21 11:25:49 -0300

    Adicionando os dals que falatvam

---
+++ WebAppLayer/DAL/AnimalDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppLayer.Models;
+
+namespace WebAppLayer.DAL
+{
+    public class AnimalDAL
+    {
+        public AnimalDAL()
+        {
+            contexto = new WildSaverContext();
+        }
+        private WildSaverContext contexto;
+
+
+        public void Adicionar(Animal a)
+        {
+            contexto.Animais.Add(a);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Animal a)
+        {
+            contexto.Update(a);
+            contexto.SaveChanges();
+
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+        public IList<Animal> Lista()
+        {
+            return contexto.Animais.ToList();
+        }
+
+        public void Remover(Animal a)
+        {
+            contexto.Animais.Remove(a);
+            contexto.SaveChanges();
+
+        }
+    }
+}
---
+++ WebAppLayer/DAL/DenunciaDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppLayer.Models;
+
+namespace WebAppLayer.DAL
+{
+    public class DenunciaDAL
+    {
+        public DenunciaDAL()
+        {
+            contexto = new WildSaverContext();
+        }
+        private WildSaverContext contexto;
+
+
+        public void Adicionar(Denuncia d)
+        {
+            contexto.Denuncias.Add(d);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Denuncia d)
+        {
+            contexto.Update(d);
+            contexto.SaveChanges();
+
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+        public IList<Denuncia> Lista()
+        {
+            return contexto.Denuncias.ToList();
+        }
+
+        public void Remover(Denuncia d)
+        {
+            contexto.Denuncias.Remove(d);
+            contexto.SaveChanges();
+
+        }
+    }
+}
