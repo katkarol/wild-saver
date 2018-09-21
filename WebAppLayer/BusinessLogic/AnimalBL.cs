1f0e49460efb907ba88104cf3baa950bd2e2aa4b
m010 <m010@benner.com.br>
2018-09-21 11:27:30 -0300

    Iniciando regra de negocio

--- WebAppLayer/BusinessLogic/AnimalBL.cs
+++ WebAppLayer/BusinessLogic/AnimalBL.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebAppLayer.DAL;
+using WebAppLayer.Models;
 
 namespace WebAppLayer.BusinessLogic
 {
     public class AnimalBL
     {
+        private AnimalDAL animalDAL; 
+        public AnimalBL()
+        {
+            animalDAL = new AnimalDAL();
+        }
 
+        public void AdicionarAnimal(Animal animal)
+        {
+            animalDAL.Adicionar(animal);
+        }
     }
 }
---
+++ WebAppLayer/BusinessLogic/DenunciaBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppLayer.DAL;
+using WebAppLayer.Models;
+
+namespace WebAppLayer.BusinessLogic
+{
+    public class DenunciaBL
+    {
+        private DenunciaDAL denunciaDAL;
+
+        public DenunciaBL()
+        {
+            this.denunciaDAL = new DenunciaDAL();
+        }
+
+        public void AdicionarDenuncia(Denuncia denuncia)
+        {
+            denunciaDAL.Adicionar(denuncia);
+
+        }
+    }
+}
