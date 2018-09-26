4414322ede6e002fbef19cbb7a2674c4b7ff3132
m010 <m010@benner.com.br>
2018-09-26 11:47:04 -0300

    Adicionando as noticias ao banco

---
+++ WebAppLayer/Migrations/20180926144519_noticias.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppLayer.Migrations
+{
+    public partial class noticias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Noticias",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(nullable: false),
+                    Resumo = table.Column<string>(nullable: false),
+                    DataPublicacao = table.Column<DateTime>(nullable: false),
+                    Link = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Noticias", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Noticias");
+        }
+    }
+}
