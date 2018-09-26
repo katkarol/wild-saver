94c5bc94423d26f60310e839fa1587329633ee32
m010 <m010@benner.com.br>
2018-09-26 11:48:24 -0300

    Adicionando menssagem de erro

--- WebAppLayer/Models/Animal.cs
+++ WebAppLayer/Models/Animal.cs
@@ -9,11 +9,11 @@ namespace WebAppLayer.Models
     public class Animal
     {
         public int ID { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Genero deve ser informado")]
         public Genero Genero { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Deve ser feita a descrição do animal")]
         public string Descricao { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O Nome Popular deve ser informado")]
         public string NomePopular { get; set; }
 
         public List<Regiao> Regioes { get; set; }
--- WebAppLayer/Models/Noticia.cs
+++ WebAppLayer/Models/Noticia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,12 @@ namespace WebAppLayer.Models
     public class Noticia
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "O Título da noticia deve ser informado")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Resumo da noticia deve ser informado")]
         public string Resumo { get; set; }
         public DateTime DataPublicacao { get; set; }
+        [Required(ErrorMessage = "O Link da noticia deve ser informado")]
         public string Link { get; set; }
     }
 }
