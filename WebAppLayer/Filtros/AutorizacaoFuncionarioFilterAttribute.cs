5bfc22d10654c7c45ad090cda525d1a5f6031887
m010 <m010@benner.com.br>
2018-09-27 11:20:17 -0300

    Adicionando filtro de acesso

---
+++ WebAppLayer/Filtros/AutorizacaoFuncionarioFilterAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebAppLayer.Models.Enums;
+
+namespace WebAppLayer.Filtros
+{
+    public class AutorizacaoFuncionarioFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            object usuario = filterContext.HttpContext.Session["USUARIO"];
+            object papelUsuario = filterContext.HttpContext.Session["PAPELUSUARIO"];
+            if (usuario == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary(
+                new { action = "Login", controller = "Usuario" }));
+            }
+            if (papelUsuario == null||(PapelUsuario)papelUsuario != PapelUsuario.Funcionario)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+               new RouteValueDictionary(
+               new { action = "Erro", controller = "Home" }));
+
+            }
+        }
+    }
+}
---
+++ WebAppLayer/Filtros/AutorizacaoUsuarioFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebAppLayer.Filtros
+{
+    public class AutorizacaoUsuarioFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            object usuario = filterContext.HttpContext.Session["USUARIO"];
+            if (usuario == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary(
+                new { action = "Login", controller = "Usuario" }));
+
+            }
+        }
+    }
+}
