16f303990170ee1e4fefe9802082e43c1e208406
m010 <m010@benner.com.br>
2018-09-28 11:46:04 -0300

    Adicionando filtro pra mudanca do nav bar

---
+++ WebAppLayer/Filtros/Mudanca-Navbar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppLayer.Models.Enums;
+
+namespace WebAppLayer.Filtros
+{
+    public class Mudanca_Navbar
+    {
+        public Mudanca_Navbar()
+        {
+        }
+        private static object papelUsuario;
+        public static bool VerificarUsuario()
+        {
+            papelUsuario = HttpContext.Current.Session["PAPELUSUARIO"];
+
+            if (papelUsuario == null || (PapelUsuario) papelUsuario == PapelUsuario.Usuario)
+            {
+                return true;
+
+            }
+            return false;
+        }
+        public static bool VerificarFuncionario()
+        {
+
+            if (papelUsuario != null &&(PapelUsuario)papelUsuario != PapelUsuario.Usuario)
+            {
+                return true;
+
+            }
+            return false;
+
+        }
+    }
+}
